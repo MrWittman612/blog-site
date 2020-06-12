055ee469822d1691362e821519baf05355ce01bd
1sfaccia1 <chad.m.wittman75@gmail.com>
2020-06-12 07:33:31 -0500

    add router and controller for users me

--- api/users/users.router.js
+++ api/users/users.router.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const User = require('../users/User.model');
 
+const me = async (req, res) => res.status(200).send(req.user);
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', me);
 
 module.exports = router;
