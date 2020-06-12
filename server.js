10d47acc811e8952bbbc29819f2b36ddd7ae0308
1sfaccia1 <chad.m.wittman75@gmail.com>
2020-06-12 07:31:30 -0500

    fix route url add protected Route to Dashboard

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 
 var connectMongoDB = require('./utils/connectMongoDB');
 
-var { login, register } = require('./api/auth');
+var { login, register, protectedRoute } = require('./api/auth');
 var usersRouter = require('./api/users/users.router');
 
 var bingNewsApi = require('./api/bingNews/bingNewsApi');
@@ -20,10 +20,9 @@ app.use(cookieParser());
 connectMongoDB();
 
 app.post('/api/login', login);
-
 app.post('/api/register', register);
 
-app.use('/users', usersRouter);
+app.use('/api/users', protectedRoute, usersRouter);
 app.use('/api', bingNewsApi);
 
 module.exports = app;
